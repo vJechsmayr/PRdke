28e254a383af55f21fda3ace0b7646b0edac7808
Overloard <marcel.gessl@gmail.com>
2018-01-11 21:34:29 +0100

    test

--- app/src/main/java/userDatabase/OperationPositions.java
+++ app/src/main/java/userDatabase/OperationPositions.java
@@ -7,6 +7,7 @@ public class OperationPositions {
 	//else send systemmessage
 	//after every step send systemmessage to next role
 	//if last position and user clicks again first button -> do it!
+	//test
 	private String name;
 	private int currentPosition;
 	private String parameter;
