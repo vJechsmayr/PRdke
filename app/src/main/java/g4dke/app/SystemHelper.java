f1654d68c6d0550ee60219f0466f6ceec7491ed2
Overloard <marcel.gessl@gmail.com>
2018-01-12 10:29:06 +0100

    OperationPosition

--- app/src/main/java/g4dke/app/SystemHelper.java
+++ app/src/main/java/g4dke/app/SystemHelper.java
@@ -7,6 +7,7 @@ import java.util.Date;
 
 import userDatabase.DBValidator;
 import userDatabase.Message;
+import userDatabase.OperationPosition;
 import userDatabase.SystemMessage;
 import userDatabase.SystemUser;
 
@@ -24,6 +25,7 @@ public  class SystemHelper {
 
 	public static final String PFAD = "C:/Users/Overloard/Flora-2/flora2";
 
+	//TODO: Strings für composed Operations müssen hier noch ausgelagert werden
 	
 	public static SystemUser getCurrentUser()
 	{
@@ -31,6 +33,33 @@ public  class SystemHelper {
 		SystemUser user = null;
 		user = DBValidator.getUser("mgesslde", "1234");
 		return user;
+	}
+	
+	//check if composed Operation is started
+	//if yes -> return object
+	//if  not -> return null
+	public static OperationPosition isComposedOperationsStarted(String nameOfOperation, String parameter, String rule, String context)
+	{
+		
+		for(OperationPosition op : DBValidator.getAllOperationPositions())
+		{
+			if(op.getName().toLowerCase().equals(nameOfOperation.toLowerCase()))
+			{
+				if(!parameter.equals("") && op.getParameter().toLowerCase().equals(parameter.toLowerCase()))
+				{
+					return op;
+				}
+				if(!rule.equals("") && op.getRule().toLowerCase().equals(rule.toLowerCase())) {
+					return op;
+				}
+				if(!context.equals("") && op.getContext().toLowerCase().equals(context.toLowerCase()))
+				{
+					return op;
+				}
+			}
+		}
+		
+		return null;
 	}
 	
 	public static void logout()
